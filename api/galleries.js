828dbe75d4b18b7f1dfc69815ed6c4511e45d4fb
noahlprince97 <noahlprince97@gmail.com>
2026-07-27 15:36:39 -0400

    Add SmugMug API handler in galleries.js

---
+++ api/galleries.js
@@ -0,0 +1,27 @@
+import oauth from "../lib/smugmug.js";
+
+export default function handler(req, res) {
+  const { accessToken, accessSecret } = req.query;
+
+  if (!accessToken || !accessSecret) {
+    return res.status(400).json({
+      error: "Missing accessToken or accessSecret",
+    });
+  }
+
+  oauth.get(
+    "https://api.smugmug.com/api/v2!authuser",
+    accessToken,
+    accessSecret,
+    function (err, data) {
+      if (err) {
+        console.error(err);
+
+        return res.status(500).json(err);
+      }
+
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(data);
+    }
+  );
+}
