26cfe9aa7c927b6cef84ef345d4fc7341c42249c
noahlprince97 <noahlprince97@gmail.com>
2026-07-27 12:05:58 -0400

    Add cookie parsing and request token handling

--- api/callback.js
+++ api/callback.js
@@ -1,28 +1,44 @@
 import axios from "axios";
 import oauth from "../lib/oauth.js";
+import { parse } from "cookie";
 
 export default async function handler(req, res) {
   try {
+    const cookies = parse(req.headers.cookie || "");
+
+    const requestTokenSecret = cookies.smugmug_request_secret;
+
+    if (!requestTokenSecret) {
+      return res.status(400).json({
+        error: "Missing request token secret."
+      });
+    }
+
     const { oauth_token, oauth_verifier } = req.query;
 
     const request = {
       url: "https://api.smugmug.com/services/oauth/1.0a/getAccessToken",
-      method: "POST",
-      data: {
-        oauth_token,
-        oauth_verifier,
-      },
+      method: "POST"
     };
 
-    const auth = oauth.authorize(request);
+    const auth = oauth.authorize(
+      request,
+      {
+        key: oauth_token,
+        secret: requestTokenSecret
+      }
+    );
 
     const response = await axios.post(
       request.url,
       null,
       {
         headers: {
-          ...oauth.toHeader(auth),
+          ...oauth.toHeader(auth)
         },
+        params: {
+          oauth_verifier
+        }
       }
     );
 
@@ -32,7 +48,7 @@ export default async function handler(req, res) {
       accessToken: params.get("oauth_token"),
       accessSecret: params.get("oauth_token_secret"),
       user: params.get("User"),
-      nickname: params.get("NickName"),
+      nickname: params.get("NickName")
     });
 
   } catch (err) {
@@ -40,7 +56,7 @@ export default async function handler(req, res) {
 
     res.status(500).json({
       error: "Failed to exchange access token",
-      details: err.response?.data || err.message,
+      details: err.response?.data || err.message
     });
   }
 }
