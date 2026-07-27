38f8def041121b892f767d77498ad305c906f011
noahlprince97 <noahlprince97@gmail.com>
2026-07-27 15:35:03 -0400

    Refactor SmugMug OAuth access token handler

--- api/callback.js
+++ api/callback.js
@@ -1,62 +1,38 @@
-import axios from "axios";
-import oauth from "../lib/oauth.js";
 import { parse } from "cookie";
+import oauth from "../lib/smugmug.js";
 
-export default async function handler(req, res) {
-  try {
-    const cookies = parse(req.headers.cookie || "");
+export default function handler(req, res) {
+  const { oauth_token, oauth_verifier } = req.query;
 
-    const requestTokenSecret = cookies.smugmug_request_secret;
+  const cookies = parse(req.headers.cookie || "");
+  const requestSecret = cookies.smugmug_secret;
 
-    if (!requestTokenSecret) {
-      return res.status(400).json({
-        error: "Missing request token secret."
-      });
-    }
-
-    const { oauth_token, oauth_verifier } = req.query;
-
-    const request = {
-      url: "https://api.smugmug.com/services/oauth/1.0a/getAccessToken",
-      method: "POST"
-    };
-
-    const auth = oauth.authorize(
-      request,
-      {
-        key: oauth_token,
-        secret: requestTokenSecret
-      }
-    );
-
-    const response = await axios.post(
-      request.url,
-      null,
-      {
-        headers: {
-          ...oauth.toHeader(auth)
-        },
-        params: {
-          oauth_verifier
-        }
-      }
-    );
-
-    const params = new URLSearchParams(response.data);
-
-    res.status(200).json({
-      accessToken: params.get("oauth_token"),
-      accessSecret: params.get("oauth_token_secret"),
-      user: params.get("User"),
-      nickname: params.get("NickName")
-    });
-
-  } catch (err) {
-    console.error(err.response?.data || err.message);
-
-    res.status(500).json({
-      error: "Failed to exchange access token",
-      details: err.response?.data || err.message
+  if (!requestSecret) {
+    return res.status(400).json({
+      error: "Missing request token secret",
     });
   }
+
+  oauth.getOAuthAccessToken(
+    oauth_token,
+    requestSecret,
+    oauth_verifier,
+    function (err, accessToken, accessSecret, results) {
+      if (err) {
+        console.error(err);
+
+        return res.status(500).json({
+          error: "Failed to exchange access token",
+          details: err,
+        });
+      }
+
+      res.status(200).json({
+        accessToken,
+        accessSecret,
+        user: results?.User,
+        nickname: results?.NickName,
+      });
+    }
+  );
 }
